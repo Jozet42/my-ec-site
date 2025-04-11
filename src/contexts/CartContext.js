c097d190c17f6dbb2b11d1bdb83de670909cbb4e
Jozet <jozet@example.com>
2025-04-11 13:29:55 +0900

    ECサイト完成版に更新

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,24 +1,34 @@
 import React, { createContext, useEffect, useState } from 'react';
-
 export const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
   const [itemAmount, setItemAmount] = useState(0);
 
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    const total = cart.reduce((accumulator, currentItem) => {
+      return accumulator + currentItem.price * currentItem.amount;
+    }, 0);
+    setTotal(total);
+  })
+
   useEffect(() => {
     if (cart) {
-      const amoount = cart.reduce((accumulator, currentItem) => {
+      const amount = cart.reduce((accumulator, currentItem) => {
         return accumulator + currentItem.amount;
-      }, 0); setItemAmount(amount);
+      }, 0);
+      setItemAmount(amount);
     }
   }, [cart]);
 
   const addToCart = (product, id) => {
     const newItem = { ...product, amount: 1 };
-    // console.log(newItem)
+    // console.log(newItem);
+    // console.log(`item ${product.title} added to the cart`);
     const cartItem = cart.find((item) => {
-      return item.id === id;
+      return item.id === id
     });
     if (cartItem) {
       const newCart = [...cart].map((item) => {
@@ -33,11 +43,11 @@ const CartProvider = ({ children }) => {
       setCart([...cart, newItem]);
     }
   }
-  console.log(cart);
+  // console.log(cart);
 
   const removeFromCart = (id) => {
     const newCart = cart.filter((item) => {
-      return item.id !== id;
+      return item.id !== id
     });
     setCart(newCart);
   };
@@ -52,13 +62,11 @@ const CartProvider = ({ children }) => {
   }
 
   const decreaseAmount = (id) => {
-    const cartItem = cart.find((item) => {
-      return item.id === id
-    });
+    const cartItem = cart.find((item) => item.id === id);
     if (cartItem) {
       const newCart = cart.map((item) => {
         if (item.id === id) {
-          return { ...item, amount: cartItem.amount - 1 }
+          return { ...item, amount: cartItem.amount - 1 };
         } else {
           return item;
         }
@@ -71,7 +79,18 @@ const CartProvider = ({ children }) => {
   }
 
   return (
-    <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart, increaseAmount, decreaseAmount, itemAmount }}>
+    <CartContext.Provider
+      value={{
+        cart,
+        addToCart,
+        removeFromCart,
+        clearCart,
+        increaseAmount,
+        decreaseAmount,
+        itemAmount,
+        total
+      }}
+    >
       {children}
     </CartContext.Provider>
   )
